17a67e2c055d19e2bfcfe61dcf05c0d1f449c1c5
nomeera <abdullahnomeer@gmail.com>
2024-02-11 07:30:48 +0200

    first commit

--- Task 1/counter_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /media/nomeer/NoMeEr/NoMeEr/Robotics Software/Khaled_Gabr/Practice/Autonomous-Robotics-Course-2/Task 1/counter_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
