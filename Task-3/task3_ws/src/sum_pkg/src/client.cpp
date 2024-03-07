f45670161a67e3bb7ccb05cfc4042ad10ff09468
nomeera <abdullahnomeer@gmail.com>
2024-03-07 10:01:58 +0200

    sum_pkg in cpp

--- Task-3/task3_ws/src/sum_pkg/src/client.cpp
+++ Task-3/task3_ws/src/sum_pkg/src/client.cpp
@@ -1,10 +1,39 @@
 #include <ros/ros.h>
+#include <sum_pkg/sum.h>
+#include <cstdlib>
 
 using namespace ros;
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+    // node
+    init(argc, argv, "sum_two_num_client", init_options::AnonymousName);
+    // handling input errors
+    if (argc != 3)
+    {
+        ROS_INFO("Usage: sum_two_num_client X Y");
+        return 1;
+    }
+    NodeHandle nh;
 
-    
+    // service and request
+    ServiceClient client = nh.serviceClient<sum_pkg::sum>("sum_two_num");
+    // waiting service
+    client.waitForExistence();
+
+    // service
+    sum_pkg::sum srv;
+    srv.request.num1 = atoll(argv[1]);
+    srv.request.num2 = atoll(argv[2]);
+
+    // handling calling errors
+    if (client.call(srv))
+        ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+    else
+    {
+        ROS_ERROR("Failed to call service sum_two_num ");
+        return 1;
+    }
+
     return 0;
 }
--- Task-3/task3_ws/src/sum_pkg/src/server.cpp
+++ Task-3/task3_ws/src/sum_pkg/src/server.cpp
@@ -1,10 +1,27 @@
 #include <ros/ros.h>
+#include <sum_pkg/sum.h>
 
 using namespace ros;
 
-int main (int argc, char **argv)
+bool handle_sum_two_num(sum_pkg::sum::Request &req,
+                        sum_pkg::sum::Response &res)
 {
+    res.sum = req.num1 + req.num2;
+    ROS_INFO("request: x=%ld, y=%ld", (long int)req.num1, (long int)req.num2);
+    ROS_INFO("sending back response: [%ld]", (long int)res.sum);
+    return true;
+}
 
-    
+int main(int argc, char **argv)
+{
+    // node
+    init(argc, argv, "sum_two_num_server", init_options::AnonymousName);
+    NodeHandle nh;
+
+    // service and response
+    ServiceServer server = nh.advertiseService("sum_two_num", handle_sum_two_num);
+    ROS_INFO("Ready to sum two num.");
+    spin();
+
     return 0;
 }
