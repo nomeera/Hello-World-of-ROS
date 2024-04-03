e7f1074064a532dbbe5ce3eb194044ff514e7ba4
nomeera <abdullahnomeer@gmail.com>
2024-04-03 02:02:14 +0200

    update dirs

--- nomeer_robot/nomeer_robot_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/nomeer/NoMeEr/NoMeEr/Robotics-Software/Khaled_Gabr/Practice/Autonomous-Robotics-Course-2/nomeer_robot/nomeer_robot_ws/src
-# Build directory: /media/nomeer/NoMeEr/NoMeEr/Robotics-Software/Khaled_Gabr/Practice/Autonomous-Robotics-Course-2/nomeer_robot/nomeer_robot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robot_gazebo")
-subdirs("robot_description")
